2914b02531a5ad48e1f089e00648d1c0e3a8c9fc
boris <nikiforov5000@gmail.com>
2023-06-06 14:04:07 +0300

    gold button

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:del_flip_card_game/controllers/game_controller.dart';
+import 'package:del_flip_card_game/models/game_timer.dart';
 import 'package:del_flip_card_game/widgets/cards_grid.dart';
 import 'package:del_flip_card_game/widgets/game_timer_widget.dart';
 import 'package:del_flip_card_game/widgets/score_progress.dart';
@@ -71,13 +72,56 @@ class _RestartButtonState extends State<RestartButton> {
         });
       },
       child: Container(
-        height: height / 20,
-        color: Colors.purpleAccent,
-        child: Center(child: Text('Restart')),
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xffF2C942),  // Start color
+              Color(0xffffdd00),  // Start color
+              Color(0xfffab00e),  // Start color
+              Color(0xff3d2601),  // Start color
+            ],
+            stops: [0.0, 0.33, 0.66, 1.0],  // Defines the position of each color
+          ),
+          border: Border.all(
+            color: Colors.brown,  // Border color
+            width: 2,  // Border width
+          ),
+          borderRadius: BorderRadius.circular(12),  // Rounded corner radius
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),  // Shadow color
+              spreadRadius: 5,  // Extends the shadow
+              blurRadius: 7,  // Soften the shadow
+              offset: Offset(0, 3),  // Position of the shadow
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            'Start Game',
+            style: TextStyle(
+              shadows: [
+                Shadow(
+                  color: Colors.brown,
+                  offset: Offset(0, 5),
+                  blurRadius: 30,
+                ),
+              ],
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+
 
 
 class GameTitle extends StatelessWidget {
