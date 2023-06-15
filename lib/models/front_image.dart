11ad52f74b0ea695e2f9a2e709698b0ee1ba6d8d
boris <nikiforov5000@gmail.com>
2023-06-15 16:13:50 +0300

    new images

--- lib/models/front_image.dart
+++ lib/models/front_image.dart
@@ -1,32 +1,22 @@
 import 'dart:math';
 
 class FrontImage {
-  Random _random = Random();
-  List<bool> _visited = List.generate(16, (index) => false);
+  final Random _random = Random();
+  final int _quantityOfFrontImages = 18;
+  List<bool> _visited = [];
+  final Map<int, String> _imagePaths = {};
 
-  Map<int, String> _imagePaths = {
-    0: 'assets/images/cards/front/whistle.jpg',
-    1: 'assets/images/cards/front/badminton.jpg',
-    2: 'assets/images/cards/front/baseball.jpg',
-    3: 'assets/images/cards/front/basketball.jpg',
-    4: 'assets/images/cards/front/bowling.jpg',
-    5: 'assets/images/cards/front/cup.jpg',
-    6: 'assets/images/cards/front/dart.jpg',
-    7: 'assets/images/cards/front/football.jpg',
-    8: 'assets/images/cards/front/golf.jpg',
-    9: 'assets/images/cards/front/medal.jpg',
-    10: 'assets/images/cards/front/podium.jpg',
-    11: 'assets/images/cards/front/pull.jpg',
-    12: 'assets/images/cards/front/shoe.jpg',
-    13: 'assets/images/cards/front/tennis.jpg',
-    14: 'assets/images/cards/front/us-football.jpg',
-    15: 'assets/images/cards/front/volley.jpg',
-  };
+  FrontImage() {
+    _visited = List.generate(_quantityOfFrontImages, (index) => false);
+    for (int i = 0; i < _quantityOfFrontImages; ++i) {
+      _imagePaths[i] = 'assets/images/cards/front/$i.png';
+    }
+  }
 
   String getRandomFrontImage() {
-    int index = _random.nextInt(16);
+    int index = _random.nextInt(_quantityOfFrontImages);
     while (_visited[index]) {
-      index = _random.nextInt(16);
+      index = _random.nextInt(_quantityOfFrontImages);
     }
     _visited[index] = true;
     return _imagePaths[index] ?? '';
